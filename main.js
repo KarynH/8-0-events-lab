692181206e9ed7fc956e018add100a3c244a12ed
Karyn Huston <huston.karyn@gmail.com>
2022-08-24 18:05:19 -0400

    lab is done

--- main.js
+++ main.js
@@ -3,7 +3,37 @@ const main = document.querySelector("main");
 for (let i = 0; i < 100; i++) {
   const div = document.createElement("div");
   div.classList.add("cell");
+  debugger
   main.append(div);
 }
 
 // You may write your code here!
+
+
+const colors= document.querySelectorAll(".color")
+for (const color of colors) {
+  color.addEventListener("click", (event) => {
+    //debugger
+    const newColor = event.target.style.backgroundColor
+    const currentColor = document.querySelector("#current-color")
+    currentColor.style.backgroundColor = newColor
+  })
+}
+
+const cells = document.querySelectorAll(".cell")
+for(const cell of cells) {
+  cell.addEventListener("click",(event) => {
+  const currentColor = document.querySelector("#current-color")
+  const newColor = currentColor.style.backgroundColor
+  event.target.style.backgroundColor = newColor
+  } )
+}
+
+
+
+
+
+// const palette = document.querySelectorAll("palette")
+// palette.addEventListener("click", () => {
+//   currentC
+// })
